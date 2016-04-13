37c944f13d6ef118a948c2e18bf55e41cbecf6b7
Marcel Voigt <mv@noch.so>
2016-04-13 20:04:43 +0200

    test: Test wrapping of long lines

---
+++ test/fixture/wrap.in.php
@@ -0,0 +1,25 @@
+<?php
+function foo($longParameterName1, $longParameterName2, $longParameterName3, $longParameterName4 = '', $longParameterName1 = '') {
+}
+$closure = function ($longParameterName1, $longParameterName2, $longParameterName3, $longParameterName4 = '', $longParameterName1 = '') {
+	$innerClosure = function ($param) use ($longUseVariable1, $longUseVariable2, $longUseVariable3, $longUseVariable4, $longUseVariable5) {
+		foo('longParameterName1', 'longParameterName2', 'longParameterName3', 'longParameterName4', 'longParameterName1');
+		$obj->foo('longParameterName1', 'longParameterName2', 'longParameterName3', 'longParameterName4', 'longParameterName1');
+		FooClass::foo('longParameterName1', 'longParameterName2', 'longParameterName3', 'longParameterName4', 'longParameterName1');
+	};
+};
+
+class X extends Y implements LongInterfaceName1, LongInterfaceName2, LongInterfaceName3, LongInterfaceName4, LongInterfaceName5, LongInterfaceName5 {
+	use SomeLongTraitName1, SomeLongTraitName2, SomeLongTraitName3, SomeLongTraitName4, SomeLongTraitName5;
+	public function foo($longParameterName1, $longParameterName2, $longParameterName3, $longParameterName4 = '', $longParameterName1 = '')	{
+	}
+}
+class Y {
+	use SomeLongTraitName1, SomeLongTraitName2, SomeLongTraitName3, SomeLongTraitName4, SomeLongTraitName5 {
+		B::smallTalk insteadof SomeLongTraitName1;
+		A::bigTalk insteadof SomeLongTraitName2;
+		B::bigTalk as talk;
+	}
+}
+interface IX extends LongInterfaceName1, LongInterfaceName2, LongInterfaceName3, LongInterfaceName4, LongInterfaceName5, LongInterfaceName5 {
+}
