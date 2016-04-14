5aaffa823d007b429912a5e9b84427db6dc59200
Marcel Voigt <mv@noch.so>
2016-04-14 22:20:34 +0200

    test: Must trim excess whitespace and lines excluding Markdown fences

---
+++ test/fixture/phpdoc.trim.in.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 
+ * Trim surrounding single blank lines
+ * 
+ */
+/**
+ * 
+ * Trim single space after anything 
+ * Do not trim exactly two space because of Markdown  
+ * Do not trim single line between text
+ * 
+ * Trim lone whitespace lines
+ *   
+ *  
+ * 
+ *
+ */
+
+/*
+ * 
+ * 
+ * 
+ * 
+ * Trim excess around single line
+ * 
+ * 
+ * 
+ */
+/**
+ * 
+ * ```
+ * 
+ * ignore fenced
+ * 
+ * 
+ * 
+ * ```
+ * 
+ * 
+ */
