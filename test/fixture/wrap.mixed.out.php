b75fdd6d0175750b1e4be941a37cb768f7833463
Marcel Voigt <mv@noch.so>
2016-04-18 22:44:57 +0200

    test: Define wrapping of grouped concatenations

--- test/fixture/wrap.mixed.out.php
+++ test/fixture/wrap.mixed.out.php
@@ -3,3 +3,7 @@ $a = 'function ' . ($node->byRef ? '&' : '') . $node->name
 	. $this->pCommaSeparatedLines($node->params, '(', ')')
 	. (null !== $node->returnType ? ' : ' . $this->pType($node->returnType) : '')
 	. ' {' . $this->pStmts($node->stmts) . "\n" . '}';
+$b = 'outside-left' . 'outside-left2'
+	. ('1111111111' . '2222222222' . '3333333333' . '4444444444' . '5555555555'
+		. '6666666666') . 'outside-right' . 'outside-right2'
+	. 'xxxxxxxxxxxxxxxxxxxx' . 'dsrfiosjf sdoifj sdofijsd fiojsdf';
