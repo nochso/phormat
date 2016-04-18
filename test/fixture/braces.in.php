f3f57b1ff9d0efbe8efce4e2918bd66310a01dce
Marcel Voigt <mv@noch.so>
2016-04-18 19:03:43 +0200

    test: Braces on same line for all class style elements

---
+++ test/fixture/braces.in.php
@@ -0,0 +1,19 @@
+<?php
+class X
+{
+	public function foo()
+	{
+	}
+}
+
+trait Y
+{
+	public function foo()
+	{
+	}
+}
+
+interface Z
+{
+	public function foo();
+}
---
+++ test/fixture/braces.out.php
@@ -0,0 +1,12 @@
+<?php
+class X {
+	public function foo() {
+	}
+}
+trait Y {
+	public function foo() {
+	}
+}
+interface Z {
+	public function foo();
+}
