544ba60803ecd429ced5b32521077dce814b6823
Marcel Voigt <mv@noch.so>
2016-04-23 20:13:40 +0200

    test: Define order of use statements

---
+++ test/fixture/use.sort.in.php
@@ -0,0 +1,12 @@
+<?php
+class X {
+}
+
+use ZZ\A;
+use Z\A;
+use B;
+use A\B\{C\D, function b\c, const D};
+use B\Bb;
+use B\A;
+use B\Aa;
+use XX\Y, XX;
---
+++ test/fixture/use.sort.out.php
@@ -0,0 +1,12 @@
+<?php
+use A\B\{function b\c, C\D, const D};
+use B;
+use B\A;
+use B\Aa;
+use B\Bb;
+use XX, XX\Y;
+use Z\A;
+use ZZ\A;
+
+class X {
+}
