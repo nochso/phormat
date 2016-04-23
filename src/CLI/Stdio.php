7b4fe82ee2e6a2aaf4bd4df666102b3373a35a58
Marcel Voigt <mv@noch.so>
2016-04-23 21:29:33 +0200

    feat: Add custom Stdio for CLI styling

---
+++ src/CLI/Stdio.php
@@ -0,0 +1,30 @@
+<?php
+namespace nochso\Phormat\CLI;
+
+class Stdio extends \Aura\Cli\Stdio {
+	public function error($error) {
+		$this->outln($this->paint(' Error ', 'redbg white') . ' ' . $this->paint($error, 'red'));
+	}
+
+	public function warn($warning) {
+		$this->outln(
+			sprintf(
+				'%s %s',
+				$this->paint(' Warning ', 'yellowbg black'),
+				$this->paint($warning, 'yellow')
+			)
+		);
+	}
+
+	public function neutral($message) {
+		$this->outln($this->paint($message, 'yellow'));
+	}
+
+	public function success($message) {
+		$this->outln($this->paint($message, 'green'));
+	}
+
+	public function paint($input, $format) {
+		return sprintf('<<%s>>%s<<reset>>', $format, $input);
+	}
+}
