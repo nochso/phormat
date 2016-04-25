e376bb36de8ef4420a2a6595374794b4aacda092
Marcel Voigt <mv@noch.so>
2016-04-25 22:14:28 +0200

    feat: Add getTerminalWidth to Stdio

--- src/CLI/Stdio.php
+++ src/CLI/Stdio.php
@@ -1,7 +1,12 @@
 <?php
 namespace nochso\Phormat\CLI;
 
+use nochso\Omni\Exec;
+use nochso\Omni\OS;
+
 class Stdio extends \Aura\Cli\Stdio {
+	private $terminalWidth;
+
 	public function error($error) {
 		$this->outln($this->paint(' Error ', 'redbg white') . ' ' . $this->paint($error, 'red'));
 	}
@@ -27,5 +32,31 @@ class Stdio extends \Aura\Cli\Stdio {
 	public function paint($input, $format) {
 		$input = str_replace('<<reset>>', '<<reset>><<' . $format . '>>', $input);
 		return sprintf('<<%s>>%s<<reset>>', $format, $input);
+	}
+
+	public function getTerminalWidth() {
+		if ($this->terminalWidth !== null) {
+			return $this->terminalWidth;
+		}
+		$this->terminalWidth = 80;
+		if (OS::isWindows()) {
+			if (preg_match('/^\s*\d+x\d+ (\d+)x\d+\s*$/', getenv('ANSICON'), $matches)) {
+				$this->terminalWidth = $matches[1];
+			} else {
+				$mode = Exec::create('mode')->run()->getOutput()[0];
+				if (preg_match('/CON.*:(\n[^|]+?){3}(?<cols>\d+)/', $mode, $matches)) {
+					$this->terminalWidth = $matches['cols'];
+				}
+			}
+		} else {
+			$stty = Exec::create('stty', '-a')->run()->getOutput();
+			foreach ($stty as $line) {
+				if (preg_match('/columns (\d+)/', $line, $matches)) {
+					$this->terminalWidth = $matches[1];
+					break;
+				}
+			}
+		}
+		return $this->terminalWidth;
 	}
 }
